0c1882bc5dfe900e3125fc5f11fc49bb6a3e95e6
gandhiruwooo <surya191199@gmail.com>
2020-11-30 16:30:24 +0700

    autentifikasi home

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,19 @@ Route::get('/', function () {
 // Rute untuk admin
 Route::middleware('auth', 'admin')->group(function () {
 	Route::get('/cetak/{id}', [SuratController::class, 'cetakSurat'])->name('cetak.surat');
+	Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+	Route::get('/psurats', [App\Http\Controllers\PSuratController::class, 'index'])->name('psurat.index');
+	Route::get('/posts', [App\Http\Controllers\PostController::class, 'index'])->name('post.index');
+	Route::get('/posts/buat', [App\Http\Controllers\PostController::class, 'create'])->name('post.create');
+	Route::post('/posts', [App\Http\Controllers\PostController::class, 'store'])->name('post.store');
+	Route::delete('/posts/{id}', [App\Http\Controllers\PostController::class, 'destroy'])->name('post.destroy');
+	Route::get('/posts/edit/{id}', [App\Http\Controllers\PostController::class, 'edit'])->name('post.edit');
+	Route::post('/posts/{id}', [App\Http\Controllers\PostController::class, 'update'])->name('post.update');
+	Route::get('/posts/show/{id}', [App\Http\Controllers\PostController::class, 'show'])->name('post.show');
+
+	Route::get('/user/tambah', [App\Http\Controllers\UserController::class, 'create'])->name('user.create');
+	Route::post('/user/tambah', [App\Http\Controllers\UserController::class, 'store'])->name('user.store');
 });
 
 // Rute untuk warga
@@ -34,16 +47,4 @@ Route::middleware('auth', 'warga')->group(function () {
 
 Auth::routes(['register' => false]);
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/psurats', [App\Http\Controllers\PSuratController::class, 'index'])->name('psurat.index');
-Route::get('/posts', [App\Http\Controllers\PostController::class, 'index'])->name('post.index');
-Route::get('/posts/buat', [App\Http\Controllers\PostController::class, 'create'])->name('post.create');
-Route::post('/posts', [App\Http\Controllers\PostController::class, 'store'])->name('post.store');
-Route::delete('/posts/{id}', [App\Http\Controllers\PostController::class, 'destroy'])->name('post.destroy');
-Route::get('/posts/edit/{id}', [App\Http\Controllers\PostController::class, 'edit'])->name('post.edit');
-Route::post('/posts/{id}', [App\Http\Controllers\PostController::class, 'update'])->name('post.update');
-Route::get('/posts/show/{id}', [App\Http\Controllers\PostController::class, 'show'])->name('post.show');
-
-Route::get('/user/tambah', [App\Http\Controllers\UserController::class, 'create'])->name('user.create');
-Route::post('/user/tambah', [App\Http\Controllers\UserController::class, 'store'])->name('user.store');
